f49d4f90aa05bab13603a8fa1936b81034492566
patrickmylius <patrickmylius@outlook.dk>
2020-10-29 20:14:06 +0100

    Project in study groups: Risk_Manager
    Creating project folder.

---
+++ Risk_Manager/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Developed by Patrick Grønvold
+ * patr6996@edu.easj.dk
+ * Student of Computer Science
+ * Zealand ErhvervsAkademi - 4700
+ * 29-10-2020
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Risk Manager 1.0");
+    }
+}
